dd134ec1045aea6e82f245477f313e75e1da6d7a
AKABOUR <akabour.said@gmail.com>
2025-05-04 17:57:34 +0200

    corriger l'imbrication de l'élément p

--- src/components/Biographie.vue
+++ src/components/Biographie.vue
@@ -9,11 +9,12 @@
             <span class="text-highlight-2">Développeur C# .NET avec plus de 10 ans d’expérience en environnement Microsoft</span>, j’ai participé à la conception, au développement et à la maintenance d’applications de gestion dans divers secteurs : gestion d’accès (déchèteries, parkings), services publics, comptabilité, enquêtes statistiques, etc.
 
 Spécialisé dans les technologies Microsoft (C#, WinForms, WPF, ASP.NET MVC), je maîtrise les architectures MVC, MVVM et n-tiers, ainsi que des outils modernes tels que Git, Azure DevOps, Docker, Grafana, et SSIS, ...
-
+</p>
 <p>À l’aise aussi bien sur des projets web (ASP.NET, ReactJS, AngularJS et VueJS) que mobiles (Android), je conçois également des logiques métiers complexes côté serveur. Curieux et passionné, j’ai récemment renforcé mes compétences en ReactJS et Node.js, avec l’objectif de les valoriser dans un nouveau challenge professionnel.
 
-Proactif, rigoureux et impliqué, j’évolue efficacement en méthodes agiles (Scrum, SAFe) et reste en veille technologique constante pour enrichir mes compétences.</p>
-        </p>
+Proactif, rigoureux et impliqué, j’évolue efficacement en méthodes agiles (Scrum, SAFe) et reste en veille technologique constante pour enrichir mes compétences.
+</p>
+        
     </div>
 </template>
 
